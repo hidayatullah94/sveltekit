81f4ce09d3de0bd0f6fcb12e3b2ec80de625168b
hidayatullah94 <hidayatullah1780@gmail.com>
2022-06-13 15:35:45 +0700

    update

--- src/routes/counter/index.svelte
+++ src/routes/counter/index.svelte
@@ -15,7 +15,7 @@
 			duration: 2, // The time in seconds for the animation to complete [seconds]
 			delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
 			once: true, // Counting at once or recount when the element in view [boolean]
-			pulse: false, // Repeat count for certain time [boolean:false|seconds]
+			pulse: false, // Repeat cogitunt for certain time [boolean:false|seconds]
 			decimals: 0, // How many decimal places to show. [uint]
 			legacy: true, // If this is true it will use the scroll event listener on browsers
 			filesizing: false, // This will enable/disable File Size format [boolean]
